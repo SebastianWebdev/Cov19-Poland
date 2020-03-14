4b6030138b51fab148e14b1350e5c7087a69671f
Sebastian Golebiowski <se.golebiowski@gmail.com>
2020-03-14 13:31:45 +0100

    Updates

---
+++ precache-manifest.5d06bff4caf71d97fbc3f29c5a2b955f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "205531542a8bda42371cad24291d2b59",
+    "url": "/Cov19-Poland/index.html"
+  },
+  {
+    "revision": "9c3473a753a7b536fb18",
+    "url": "/Cov19-Poland/static/css/main.98476885.chunk.css"
+  },
+  {
+    "revision": "d85a909da90ff24ee379",
+    "url": "/Cov19-Poland/static/js/2.56baba65.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Cov19-Poland/static/js/2.56baba65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c3473a753a7b536fb18",
+    "url": "/Cov19-Poland/static/js/main.60b889e1.chunk.js"
+  },
+  {
+    "revision": "6342ecaba590c06c1dcd",
+    "url": "/Cov19-Poland/static/js/runtime-main.c4e8469f.js"
+  }
+]);
